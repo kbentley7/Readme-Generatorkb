af6e696c6900180edeb75e0b9360963e165f7fb5
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-09-30 08:38:05 -0400

    placed inquirer prompt

--- index.js
+++ index.js
@@ -6,3 +6,18 @@ var generateMarkdown = require("./utils/generateMarkdown");
 function writeToFile(filename, data) {
     fs.writerFileSync(path.join(process.cwd(),fileName), data);
 }
+
+inquirer.prompt([
+    {
+
+      type: "input",
+      name: "username",
+      message: "Enter Your Github Username"
+    },
+
+    {
+        // Takes user text input
+        type: "input",
+        name: "email",
+        message: "Enter Your Email Address"
+      },
